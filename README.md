76bd94dba9113331e68af589cf21103181802aa9
gungos <zbigiz@o2.pl>
2021-09-02 17:47:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# puj2rz-rj8zey
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/puj2rz-rj8zey)
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { enableRipple } from '@syncfusion/ej2-base';
+enableRipple(true);
+
+import { DocumentEditorContainer, Toolbar } from '@syncfusion/ej2-documenteditor';
+import { TitleBar } from './title-bar';
+import * as data from './data-default.json';
+
+
+/**
+ * Default document editor sample
+ */
+
+    
+    let hostUrl: string = 'https://ej2services.syncfusion.com/production/web-services/';
+
+    let container: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true, height: '590px' });
+    DocumentEditorContainer.Inject(Toolbar);
+    container.serviceUrl = hostUrl + 'api/documenteditor/';
+    container.appendTo('#container');
+
+    let titleBar: TitleBar = new TitleBar(document.getElementById('documenteditor_titlebar'), container.documentEditor, true);
+    container.documentEditor.open(JSON.stringify((<any>data)));
+    container.documentEditor.documentName = 'Getting Started';
+    titleBar.updateDocumentTitle();
+
+    container.documentChange = (): void => {
+        titleBar.updateDocumentTitle();
+        container.documentEditor.focusIn();
+    };
+
+    container.contentChange = ($event): void => {
+        console.log($event.name);
+    };
+
+
